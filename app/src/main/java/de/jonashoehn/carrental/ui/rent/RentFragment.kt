63c1a4f8fbde3c63ccb176c8386537c2fafad464
Stefan Höhn <stefan@andreaundstefanhoehn.de>
2022-01-16 13:48:27 +0100

    fix abstract class

--- app/src/main/java/de/jonashoehn/carrental/ui/rent/RentFragment.kt
+++ app/src/main/java/de/jonashoehn/carrental/ui/rent/RentFragment.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import de.jonashoehn.carrental.databinding.FragmentRentBinding
 
-abstract class RentFragment : Fragment() {
+class RentFragment : Fragment() {
 
     private lateinit var homeViewModel: RentViewModel
     private var _binding: FragmentRentBinding? = null
